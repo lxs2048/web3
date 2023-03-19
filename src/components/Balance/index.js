cd9e31a5dfc3eeda2a22a8d56ffbcb1b3bbc320a
zhangsan <lxs2048@sina.com>
2023-03-19 22:36:37 +0800

    事件订阅

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -3,6 +3,7 @@ import { Card, Col, Row, Statistic, Descriptions } from 'antd';
 import useConnectSol from '../../hooks/useConnectSol'
 import useReduxStore from '../../hooks/useReduxStore'
 import { loadBalanceData } from '../../redux/slices/balanceSlice'
+import { loadOrderLists } from '../../redux/slices/orderSlice';
 export const convert = (n) => {
   if (!n || !window.WEB) return ''
   return window.WEB.web3.utils.fromWei(n, "ether")
@@ -12,6 +13,19 @@ function Balance() {
   const [state, dispatch] = useReduxStore(state => state.balance)
   useEffect(() => {
     initData.web3 && dispatch(loadBalanceData(initData))
+    if(initData.Instance){
+      const exchange = initData.Instance.exchangeInstance
+      exchange.events.Order({},(error,event)=>{
+        dispatch(loadOrderLists(initData))
+      })
+      exchange.events.Cancel({},(error,event)=>{
+        dispatch(loadOrderLists(initData))
+      })
+      exchange.events.Trade({},(error,event)=>{
+        dispatch(loadOrderLists(initData))
+        dispatch(loadBalanceData(initData))
+      })
+    }
   }, [initData])
   const { TokenWallet, TokenExchange, EtherWallet, EtherExchange } = state || {}
   return (

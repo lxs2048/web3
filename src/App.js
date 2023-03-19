f4bb33e70bf416ae6de2eb0de12dac057d5e9968
zhangsan <lxs2048@sina.com>
2023-03-19 11:46:47 +0800

    初始化组件

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import obj from './build/DolToken.json';
-console.log(obj,'obj');
+import Balance from "./components/Balance";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Balance/>
     </div>
   );
 }
